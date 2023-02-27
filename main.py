7bc4af42fd587d81f98fcfb16e5571e328fdb4fb
alpdevv <>
2023-02-27 06:58:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to the band name generator.")
+
+city = input("What's name of the city you grew up in?\n")
+
+pet = input("What's name of a pet?\n")
+
+print("Your band name could be " + city + " " + pet)
+
